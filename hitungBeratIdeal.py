c0d9bfad479e42b39b38848515a7159c55a575b2
hendraestu <hendraep83@gmail.com>
2022-03-31 11:57:51 +0700

    001

--- hitungBeratIdeal.py
+++ hitungBeratIdeal.py
@@ -3,6 +3,8 @@ from flask import jsonify, request
 
 app = Flask(__name__)
 
+#curl -i -X POST http://127.0.0.1:7092/api/v1/bmi -d "berat=65&tinggi=175"
+
 @app.route("/api/v1/bmi", methods=["POST"])
 def hitung():
     berat = float(request.form['berat'])
